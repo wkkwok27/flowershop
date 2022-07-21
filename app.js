99700967bc88a4f278a17ff05bbfcb0932b7ef20
chi <chi@heyavo.com>
2022-07-21 21:26:09 +0800

    init

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const session  = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const validator = require('express-validator');
+const MongoStore = require('connect-mongo');
+const {mongoDbUrl} = require('./config/env');
+
+const mainRoute = require('./routes/main');
+const userRoutes = require('./routes/user');
+const checkoutRoutes = require('./routes/checkout');
+const cartRoutes = require('./routes/cart');
+
+const app = express();
+
+mongoose.connect(mongoDbUrl);
+require('./config/passport');
+
+
+// engine setup
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(validator());
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: mongoDbUrl
+    }),
+    cookie: {maxAge: 180 * 60 * 1000}
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+   res.locals.login = req.isAuthenticated();
+   res.locals.session = req.session;
+   next();
+});
+
+
+app.use('/', mainRoute);
+app.use('/user', userRoutes);
+app.use('/', checkoutRoutes);
+app.use('/', cartRoutes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
