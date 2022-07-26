70d663a5e928292f3a5a620453627b99a6d50f9d
g <wkkwok27@gmail.com>
2022-07-26 23:22:19 +0800

    updates

--- routes/checkout.js
+++ routes/checkout.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const Order = require('../models/order');
 const Cart = require('../models/cart');
-const {stripeSecretKey} = require('../config/env');
 
 router.get('/checkout', isLoggedIn, function (req, res, next) {
     if(!req.session.cart) {
@@ -20,30 +19,17 @@ router.post('/checkout', isLoggedIn, function(req, res, next) {
     }
     const cart = new Cart(req.session.cart);
 
-    const stripe = require('stripe')(stripeSecretKey);
-
-    stripe.charges.create({
-        amount: cart.totalPrice * 100,
-        currency: 'usd',
-        source: req.body.stripeToken, // obtained with Stripe.js
-        description: 'Test Charge'
-    }, function(err, charge) {
-        if(err) {
-            req.flash('error', err.message);
-            return res.redirect('/checkout');
-        }
-        const order = new Order({
-            user: req.user,
-            cart: cart,
-            address: req.body.address,
-            name: req.body.name,
-            paymentId: charge.id
-        });
-        order.save(function(err, result) {
-            req.flash('success', 'Successfully bought product!');
-            req.session.cart = null;
-            res.redirect('/');
-        });
+    const order = new Order({
+        user: req.user,
+        cart: cart,
+        address: req.body.address,
+        name: req.body.name,
+        paymentId: '123'
+    });
+    order.save(function(err, result) {
+        req.flash('success', 'Successfully bought product!');
+        req.session.cart = null;
+        res.redirect('/');
     });
 });
 
